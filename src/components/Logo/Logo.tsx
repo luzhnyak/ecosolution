ec36ac9729f01e3da439cdecaafa76195aa32b16
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-06-30 18:19:47 +0300

    add logo

--- src/components/Logo/Logo.tsx
+++ src/components/Logo/Logo.tsx
@@ -1,10 +1,18 @@
+import css from "./Logo.module.css";
+
 function Logo() {
   return (
-    <div>
-      <img src="../../../public/icons/logo.svg" alt="" />
-      <span>ecosolution</span>
-      <span>
-        <span>GREEN</span>ERGY FOR LIFE
+    <div className={css.wrapper}>
+      <img
+        className={css.img}
+        src="/icons/logo.svg"
+        alt="Ecosolution"
+        width={31}
+        height={18}
+      />
+      <span className={css.title}>ecosolution</span>
+      <span className={css.text}>
+        <span className={css.green}>GREEN</span>ERGY FOR LIFE
       </span>
     </div>
   );
