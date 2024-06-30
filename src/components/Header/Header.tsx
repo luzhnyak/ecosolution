b460febf597e05bc5ed48f850b413a0f0e74105c
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2024-06-30 08:45:40 +0300

    init vite react app

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,11 @@
+import Logo from "../Logo/Logo";
+
+const Header = () => {
+  return (
+    <div>
+      <Logo />
+    </div>
+  );
+};
+
+export default Header;
---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,13 @@
+function Logo() {
+  return (
+    <div>
+      <img src="../../../public/icons/logo.svg" alt="" />
+      <span>ecosolution</span>
+      <span>
+        <span>GREEN</span>ERGY FOR LIFE
+      </span>
+    </div>
+  );
+}
+
+export default Logo;
